e527c9b584404b106f9d051f628420cf30b6588f
Zach Barham <git@alduino.dev>
2020-12-10 19:58:53 +1000

    Add connections between two segments

--- src/lib/road-2-main.ts
+++ src/lib/road-2-main.ts
@@ -24,16 +24,41 @@ const GRID_SIZE = 30;
 const GRID_FALLOFF = 5;
 
 type SegmentIdentifier = ReturnType<typeof uuid>;
+type SegmentConnectionIdentifier = ReturnType<typeof uuid>;
+
+interface SegmentConnection {
+    id: SegmentConnectionIdentifier;
+
+    // The point of the connection. Linked to the correct point on each segment
+    point: Vector2;
+
+    // A list of segment identifiers that are connected
+    segments: SegmentIdentifier[];
+}
 
 interface Segment {
     id: SegmentIdentifier;
     shape: Bezier;
-    startConnections: SegmentIdentifier[];
-    endConnections: SegmentIdentifier[];
+    startConnection?: SegmentConnection;
+    endConnection?: SegmentConnection;
+
+    startPoint: Vector2;
+    controlPoint: Vector2;
+    endPoint: Vector2;
+
+    _moveState?: SegmentModificationInfo;
 }
 
 const segments: Map<SegmentIdentifier, Segment> = new Map();
+const connections: Map<SegmentConnectionIdentifier, SegmentConnection> = new Map();
 let workingSegment: Segment;
+
+// @ts-ignore
+window.devTool_reloadSegmentMap = () => {
+    console.clear();
+    console.log(segments);
+    console.log(connections);
+}
 
 const KEYS = {
     noGrid: "Shift",
@@ -73,6 +98,14 @@ function getActionCurve(mousePos: Vector2) {
     return null;
 }
 
+function getActionConnection(mousePos: Vector2) {
+    for (const connection of connections.values()) {
+        if (connection.point.distance(mousePos) < SNAP_DISTANCE) return connection;
+    }
+
+    return null;
+}
+
 function getSnapLines(skip: Bezier[] = []) {
     return iter(segments.values()).map(({shape}) => {
         if (skip.includes(shape)) return [];
@@ -90,28 +123,183 @@ function getSnapLines(skip: Bezier[] = []) {
     }).flat(v => v);
 }
 
-let modifySegment: Segment,
-    modifyPoint: Point, oppositePoint: Vector2, startMid: Vector2,
-    connections: SegmentIdentifier[], connectionPoint: "start" | "end",
-    startPointAngle: number, startPointDist: number;
+interface SegmentModificationInfo {
+    points: {
+        point: Point;
+        opposite: Vector2;
+        mid: Vector2;
+    };
 
-function deleteSegment(segment: Segment) {
-    segments.delete(segment.id);
+    connection: SegmentConnection;
 
-    for (const otherSegmentId of segment.startConnections) {
-        const otherSegment = segments.get(otherSegmentId);
-        const index = otherSegment.endConnections.indexOf(segment.id);
-        otherSegment.endConnections.splice(index);
-    }
-
-    for (const otherSegmentId of segment.endConnections) {
-        const otherSegment = segments.get(otherSegmentId);
-        const index = otherSegment.startConnections.indexOf(segment.id);
-        otherSegment.startConnections.splice(index);
+    start: {
+        angle: number;
+        dist: number;
     }
 }
 
+type StartOrEndConnection = "startConnection" | "endConnection";
+type StartOrEndPoint = "startPoint" | "endPoint";
+
+function connect(a: Segment, b: Segment, aEnd: "start" | "end", bEnd: "start" | "end") {
+    if (!a || !b) throw new Error("A and B must both be defined");
+    if (a.id === b.id) throw new Error("A and B must be different");
+
+    const aConnectionKey = aEnd + "Connection" as StartOrEndConnection;
+    const bConnectionKey = bEnd + "Connection" as StartOrEndConnection;
+
+    const aPointKey = aEnd + "Point" as StartOrEndPoint;
+    const bPointKey = bEnd + "Point" as StartOrEndPoint;
+
+    const aConnection = a[aConnectionKey];
+    const bConnection = b[bConnectionKey];
+
+    // they are already connected
+    if (aConnection && aConnection === bConnection) return aConnection;
+
+    if (aConnection && bConnection) {
+        throw new Error("One of A or B must not be connected");
+    }
+
+    if (aConnection && !bConnection) {
+        // b is not connected to anything
+        b[bConnectionKey] = aConnection;
+
+        // link the point
+        b[bPointKey] = aConnection.point;
+
+        aConnection.segments.push(b.id);
+
+        return aConnection;
+    } else if (bConnection && !aConnection) {
+        // a is not connected to anything
+        a[aConnectionKey] = bConnection;
+
+        bConnection.segments.push(a.id);
+
+        // link the point
+        a[aPointKey] = aConnection.point;
+
+        return bConnection;
+    } else {
+        // neither are connected to anything
+        const connection: SegmentConnection = {
+            id: uuid(),
+            // use a's point - they usually will be the same
+            point: a[aPointKey],
+            segments: [a.id, b.id]
+        };
+
+        connections.set(connection.id, connection);
+        a[aConnectionKey] = connection;
+        b[bConnectionKey] = connection;
+
+        // link the point
+        b[bPointKey].replace(connection.point);
+
+        return connection;
+    }
+}
+
+function disconnect(connection: SegmentConnection, segment: Segment) {
+    // no point disconnecting from a connection that doesn't exist
+    if (!connection) return;
+
+    if (!connection.segments.includes(segment.id)) throw new Error("Segment is not part of connection");
+
+    connection.segments.splice(connection.segments.indexOf(segment.id), 1);
+
+    if (connection.segments.length <= 1) connections.delete(connection.id);
+}
+
+function startConnectionModification(connection: SegmentConnection) {
+    for (const segmentId of connection.segments) {
+        const segment = segments.get(segmentId);
+        const oppositePoint = connection.id === segment.startConnection?.id ? segment.endPoint : segment.startPoint;
+        const midPoint = segment.controlPoint.clone();
+
+        const angle = connection.point.angleTo(oppositePoint);
+        const dist = connection.point.distance(oppositePoint);
+
+        segment._moveState = {
+            points: {
+                point: connection.point,
+                opposite: oppositePoint,
+                mid: midPoint
+            },
+
+            connection,
+
+            start: {
+                angle,
+                dist
+            }
+        };
+    }
+}
+
+/**
+ * Moves the connection point, and scales any connected segments to keep the same shape
+ */
+function modifyConnection(connection: SegmentConnection, targetPoint: Vector2) {
+    targetPoint.assignTo(connection.point);
+
+    for (const segmentId of connection.segments) {
+        const segment = segments.get(segmentId);
+        const {_moveState: state} = segment;
+
+        const angleDiff = state.points.opposite.angleTo(targetPoint) - state.start.angle;
+        const distMul = state.points.opposite.distance(targetPoint) / state.start.dist;
+
+        const originalMidAngle = state.points.mid.angleTo(state.points.opposite);
+        const originalMidDist = state.points.mid.distance(state.points.opposite);
+
+        const newMid = Vector2.fromDir(originalMidAngle + angleDiff)
+            .withLength(originalMidDist * distMul).add(state.points.opposite);
+
+        newMid.assignTo(segment.controlPoint);
+    }
+}
+
+function deleteSegment(segment: Segment) {
+    disconnect(segment.startConnection, segment);
+    disconnect(segment.endConnection, segment);
+    segments.delete(segment.id);
+}
+
+let modifySegment: Segment, modifyConnect: SegmentConnection;
 function handleCurvePre(ctx: CanvasFrameContext, oldState: CurveMakerState): CurveMakerState {
+    const actionConnection = getActionConnection(ctx.mousePos);
+    if (actionConnection || modifyConnect) {
+        const conn = actionConnection || modifyConnect;
+
+        circle(ctx, conn.point, 7, {
+            thickness: 2,
+            colour: HighlightType.Modify,
+            dash: [3, 2]
+        });
+
+        if (ctx.mousePressed.left) {
+            modifyConnect = actionConnection || modifyConnect;
+            startConnectionModification(modifyConnect);
+        }
+
+        if (ctx.mouseDown.left) {
+            let target = ctx.mousePos;
+
+            if (isUsingGrid(ctx)) {
+                drawGrid(ctx);
+                target = snapToGrid(ctx.mousePos);
+            }
+
+            modifyConnection(modifyConnect, target);
+        }
+
+        if (ctx.mouseReleased.left) modifyConnect = null;
+
+        return oldState;
+    }
+
     const actionSegment = getActionCurve(ctx.mousePos);
     if (actionSegment && ctx.keyDown.get(KEYS.delete)) {
         drawCurve(ctx, actionSegment.shape, false, HighlightType.Delete);
@@ -125,109 +313,40 @@ function handleCurvePre(ctx: CanvasFrameContext, oldState: CurveMakerState): Cur
         const mCurve0 = Vector2.from(modifySegment.shape.point(0));
         const mCurve2 = Vector2.from(modifySegment.shape.point(2));
 
-        if (ctx.mousePressed.left) {
-            if (ctx.mousePos.distance(mCurve0) < SNAP_DISTANCE) {
-                modifyPoint = modifySegment.shape.point(0);
-                oppositePoint = mCurve2;
-                connections = modifySegment.startConnections;
-                connectionPoint = "end";
+        let targetPos = ctx.mousePos, snapped = false;
+
+        if (!ctx.keyDown.get(KEYS.noSnap)) {
+            const snapLines = getSnapLines([modifySegment.shape])
+                .concat(new Bezier([mCurve0, Vector2.lerp(mCurve0, mCurve2, .5), mCurve2]))
+                .toArray();
+
+            for (const line of snapLines) {
+                drawCurve(ctx, line, false, HighlightType.Snap);
             }
 
-            if (ctx.mousePos.distance(mCurve2) < SNAP_DISTANCE) {
-                modifyPoint = modifySegment.shape.point(2);
-                oppositePoint = mCurve0;
-                connections = modifySegment.endConnections;
-                connectionPoint = "start";
-            }
+            for (const curve of snapLines) {
+                const projectPoint = Vector2.from(curve.project(ctx.mousePos));
 
-            if (modifyPoint) {
-                const pointVec = Vector2.from(modifyPoint);
-                startPointAngle = pointVec.angleTo(oppositePoint);
-                startPointDist = pointVec.distance(oppositePoint);
-                startMid = Vector2.from(modifySegment.shape.point(1));
+                if (ctx.mousePos.distance(projectPoint) < SNAP_DISTANCE) {
+                    targetPos = projectPoint;
+                    snapped = true;
+                }
             }
         }
 
-        if (modifyPoint) {
-            circle(ctx, Vector2.from(modifyPoint), 7, {
+        if (!snapped && isUsingGrid(ctx) && ctx.mouseDown.left) {
+            drawGrid(ctx);
+            targetPos = snapToGrid(targetPos);
+        }
+
+        if (ctx.mouseDown.left) {
+            targetPos.assignTo(modifySegment.shape.point(1));
+
+            circle(ctx, targetPos, 7, {
                 thickness: 2,
                 colour: HighlightType.Modify,
                 dash: [3, 2]
             });
-
-            if (ctx.mouseDown.left) {
-                let target = ctx.mousePos;
-
-                if (isUsingGrid(ctx)) {
-                    drawGrid(ctx);
-                    target = snapToGrid(target);
-                }
-
-                const pointAngleDiff = oppositePoint.angleTo(target) - startPointAngle;
-                const pointDistMul = oppositePoint.distance(target) / startPointDist;
-
-                const originalMidAngle = startMid.angleTo(oppositePoint);
-                const originalMidDist = startMid.distance(oppositePoint);
-
-                const newMid = Vector2.fromDir(originalMidAngle + pointAngleDiff)
-                    .withLength(originalMidDist * pointDistMul).add(oppositePoint);
-
-                // not the best way to do it, as points are meant to be readonly
-                // however it is simpler than storing which one to write to somewhere
-                target.assignTo(modifyPoint);
-                newMid.assignTo(modifySegment.shape.point(1));
-
-                for (const connectionId of connections) {
-                    const connectionSeg = segments.get(connectionId);
-                    if (connectionPoint === "start") {
-                        target.assignTo(connectionSeg.shape.point(0));
-                    } else {
-                        target.assignTo(connectionSeg.shape.point(2));
-                    }
-                }
-            }
-
-            if (ctx.mouseReleased.left) {
-                modifyPoint = null;
-                oppositePoint = null;
-                connections = null;
-            }
-        } else {
-            let targetPos = ctx.mousePos, snapped = false;
-
-            if (!ctx.keyDown.get(KEYS.noSnap)) {
-                const snapLines = getSnapLines([modifySegment.shape])
-                    .concat(new Bezier([mCurve0, Vector2.lerp(mCurve0, mCurve2, .5), mCurve2]))
-                    .toArray();
-
-                for (const line of snapLines) {
-                    drawCurve(ctx, line, false, HighlightType.Snap);
-                }
-
-                for (const curve of snapLines) {
-                    const projectPoint = Vector2.from(curve.project(ctx.mousePos));
-
-                    if (ctx.mousePos.distance(projectPoint) < SNAP_DISTANCE) {
-                        targetPos = projectPoint;
-                        snapped = true;
-                    }
-                }
-            }
-
-            if (!snapped && isUsingGrid(ctx) && ctx.mouseDown.left) {
-                drawGrid(ctx);
-                targetPos = snapToGrid(targetPos);
-            }
-
-            if (ctx.mouseDown.left) {
-                targetPos.assignTo(modifySegment.shape.point(1));
-
-                circle(ctx, targetPos, 7, {
-                    thickness: 2,
-                    colour: HighlightType.Modify,
-                    dash: [3, 2]
-                });
-            }
         }
 
         drawCurve(ctx, modifySegment.shape, true, HighlightType.Modify);
@@ -309,14 +428,32 @@ function drawExistingCurves(ctx: CanvasFrameContext) {
     for (const {shape} of segments.values()) {
         drawCurve(ctx, shape, false);
     }
+
+    for (const connection of connections.values()) {
+        circle(ctx, connection.point, 5, {
+            thickness: 2,
+            fill: HighlightType.None
+        });
+    }
 }
 
 function createSegment(shape: Bezier): Segment {
+    // create vec2s from the points, then set the points back to the vec2s
+    // this allows us to set the value of the vec2 and have it automatically go to the point, making life much easier
+    // as any "connected" points all actually use the exact same vec2 instance as their point, so we only have to set
+    // the new position at once place for it to propagate everywhere
+    const startPoint = Vector2.import(shape.point(0));
+    const controlPoint = Vector2.import(shape.point(2));
+    const endPoint = Vector2.import(shape.point(3));
+
+    shape.points[0] = startPoint;
+    shape.points[1] = controlPoint;
+    shape.points[2] = endPoint;
+
     return {
         id: uuid(),
         shape,
-        startConnections: [],
-        endConnections: []
+        startPoint, controlPoint, endPoint
     };
 }
 
@@ -422,10 +559,10 @@ function handleCurveEnd(ctx: CanvasFrameContext, oldState: CurveMakerState): Cur
             // finish this curve and start a new one at the end point
             segments.set(workingSegment.id, workingSegment);
             const previousSegment = workingSegment;
+
             workingSegment = createSegment(new Bezier([targetPoint, new Vector2(), new Vector2()]));
 
-            workingSegment.startConnections.push(previousSegment.id);
-            previousSegment.endConnections.push(workingSegment.id);
+            connect(previousSegment, workingSegment, "end", "start");
 
             return CurveMakerState.End;
         }
